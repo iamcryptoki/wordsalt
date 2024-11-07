b853be1969d6b0dfa274a56964e21451d231f89a
Gael Gentil <github@cryptoki.dev>
2024-11-07 12:49:10 +0100

    Add wordsalt.go.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/iamcryptoki/wordsalt
+
+go 1.20
---
+++ wordsalt.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+)
+
+const (
+	// keyLen defines the length of the generated keys.
+	keyLen = 64
+	// chars defines the set of characters allowed in the keys.
+	chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_ []{}<>~`+=,.;:/?|"
+)
+
+var (
+	// keyNames holds the names of the WordPress keys to generate.
+	keyNames = []string{
+		"AUTH_KEY",
+		"SECURE_AUTH_KEY",
+		"LOGGED_IN_KEY",
+		"NONCE_KEY",
+		"AUTH_SALT",
+		"SECURE_AUTH_SALT",
+		"LOGGED_IN_SALT",
+		"NONCE_SALT",
+	}
+)
+
+// GenerateWordPressKeys generates a map of WordPress security keys.
+func GenerateWordPressKeys() map[string]string {
+	keys := make(map[string]string)
+
+	for _, name := range keyNames {
+		key, err := generateKey(keyLen) // generateKey returns an error now
+		if err != nil {
+			panic(fmt.Errorf("failed to generate key %s: %w", name, err))
+		}
+
+		keys[name] = key
+	}
+
+	return keys
+}
+
+// generateKey generates a random key with the specified length and allowed characters.
+func generateKey(length int) (string, error) {
+	if length <= 0 {
+		return "", fmt.Errorf("invalid key length: %d", length)
+	}
+
+	key := make([]byte, length)
+	for i := range key {
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
+		if err != nil {
+			return "", fmt.Errorf("failed to generate random number: %w", err)
+		}
+
+		key[i] = chars[n.Int64()]
+	}
+
+	return string(key), nil
+}
+
+func main() {
+	keys := GenerateWordPressKeys()
+
+	for _, name := range keyNames {
+		fmt.Printf("define('%s', '%s');\n", name, keys[name])
+	}
+}
